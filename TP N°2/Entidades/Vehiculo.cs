ce9db269429db8155bfd25dba32a6f165e910652
ferUTN <masotti.utn@gmail.com>
2020-09-30 20:44:00 -0300

    Modificada la clase Vehiculo

--- TP N°2/Entidades/Vehiculo.cs
+++ TP N°2/Entidades/Vehiculo.cs
@@ -9,44 +9,67 @@ namespace Entidades
     /// <summary>
     /// La clase Vehiculo no deberá permitir que se instancien elementos de este tipo.
     /// </summary>
-    public sealed class Vehiculo
+    public abstract class Vehiculo
     {
-        enum EMarca
+        #region enumerados
+        public enum EMarca
         {
             Chevrolet, Ford, Renault, Toyota, BMW, Honda, HarleyDavidson
         }
-        enum ETamanio
+        public enum ETamanio
         {
             Chico, Mediano, Grande
         }
-        EMarca marca;
-        string chasis;
-        ConsoleColor color;
+        #endregion
+
+        #region atributos y propiedades
+
+        private string chasis;
+        private ConsoleColor color;
+        private EMarca marca;
 
         /// <summary>
         /// ReadOnly: Retornará el tamaño
         /// </summary>
-        abstract ETamanio Tamanio { get; set; }
+        protected abstract ETamanio Tamanio { get; }
+        #endregion
 
+       
+        #region constructor
+        /// <summary>
+        /// Constructor sobrecargado
+        /// </summary>
+        /// <param name="chasis">Recibe el chasis del vehículo</param>
+        /// <param name="marca">Recibe la marca del vehículo</param>
+        /// <param name="color">Recibe el color del vehículo</param>
+        public Vehiculo(string chasis, EMarca marca, ConsoleColor color)
+        {
+            this.chasis = chasis;
+            this.marca = marca;
+            this.color = color;
+        }
+        #endregion
+
+        #region metodos
         /// <summary>
         /// Publica todos los datos del Vehiculo.
         /// </summary>
         /// <returns></returns>
-        sealed string Mostrar()
+        public virtual string Mostrar()
         {
-            return this;
+            return (string)this;
         }
+        #endregion
 
-        private static explicit operator string(Vehiculo p)
+        #region sobrecargas
+        public static explicit operator string(Vehiculo p)
         {
             StringBuilder sb = new StringBuilder();
-
-            sb.AppendLine("CHASIS: {0}\r\n", p.chasis);
-            sb.AppendLine("MARCA : {0}\r\n", p.marca.ToString());
-            sb.AppendLine("COLOR : {0}\r\n", p.color.ToString());
+            sb.AppendFormat("CHASIS: {0}\n", p.chasis);
+            sb.AppendFormat("MARCA : {0}\n", p.marca.ToString());
+            sb.AppendFormat("COLOR : {0}\n", p.color.ToString());
             sb.AppendLine("---------------------");
-
-            return sb;
+            return sb.ToString();
         }
 
         /// <summary>
@@ -57,8 +80,14 @@ namespace Entidades
         /// <returns></returns>
         public static bool operator ==(Vehiculo v1, Vehiculo v2)
         {
-            return (v1.chasis == v2.chasis);
+            bool aux = false;
+            if (v1.chasis == v2.chasis)
+            {
+                aux = true;
+            }
+            return aux;
         }
+
         /// <summary>
         /// Dos vehiculos son distintos si su chasis es distinto
         /// </summary>
@@ -67,7 +96,9 @@ namespace Entidades
         /// <returns></returns>
         public static bool operator !=(Vehiculo v1, Vehiculo v2)
         {
-            return (v1.chasis == v2.chasis);
+            return !(v1 == v2);
         }
+        #endregion
+
     }
 }
