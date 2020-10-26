d3b1888040cbfdbecfef46a5055328e8cdfee80b
ferUTN <masotti.utn@gmail.com>
2020-10-25 23:42:09 -0300

    TP 3 - Agregada la clase Persona

---
+++ Masotti.Fernando.2A.TP3/Clases Abstractas/Persona.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excepciones;
+using System.Text.RegularExpressions;
+using System.Data.Odbc;
+
+namespace EntidadesAbstractas
+{
+    public abstract class Persona
+    {
+
+
+        public enum ENacionalidad
+        {
+            Argentino,
+            Extranjero
+        }
+
+
+
+        private string apellido;
+        private int dni;
+        private ENacionalidad nacionalidad;
+        private string nombre;
+
+        public string Apellido
+        {
+            get
+            {
+                return this.apellido;
+            }
+            set
+            {
+                this.apellido = this.ValidarNombreApellido(value);
+            }
+        }
+
+        public int DNI
+        {
+            get
+            {
+                return this.dni;
+            }
+            set
+            {
+                this.dni = this.ValidarDni(this.Nacionalidad, value);
+            }
+        }
+
+        public ENacionalidad Nacionalidad
+        {
+            get
+            {
+                return this.nacionalidad;
+            }
+            set
+            {
+                this.nacionalidad = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+            set
+            {
+                this.nombre = this.ValidarNombreApellido(value);
+            }
+        }
+
+        public string StringToDNI
+        {
+            set
+            {
+                this.dni = this.ValidarDni(this.Nacionalidad, value);
+            }
+        }
+
+
+        public Persona()
+        {
+
+        }
+
+        public Persona(string nombre, string apellido, ENacionalidad nacionalidad):this()
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Nacionalidad = nacionalidad;
+        }
+
+        public Persona(string nombre, string apellido, int dni, ENacionalidad nacionalidad) : this(nombre, apellido, nacionalidad)
+        {
+            this.DNI = dni;
+        }
+
+        public Persona(string nombre, string apellido, string dni, ENacionalidad nacionalidad) : this(nombre, apellido, nacionalidad)
+        {
+            this.StringToDNI = dni;
+        }
+
+
+        private int ValidarDni(ENacionalidad nacionalidad, int dato)
+        {
+            switch (nacionalidad)
+            {
+                case ENacionalidad.Argentino:
+                    if (dato < 1 || dato > 89999999)
+                    {
+                        throw new NacionalidadInvalidaException("Nro de DNI incorrecto para un argentino");
+                    }
+                    break;
+                case ENacionalidad.Extranjero:
+                    if (dato < 90000000 || dato > 99999999)
+                    {
+                        throw new NacionalidadInvalidaException("Nro de DNI incorrecto para un extranjero");
+                    }
+                    break;
+            }
+            return dato;
+        }
+
+        private int ValidarDni(ENacionalidad nacionalidad, string dato)
+        {
+            int dni = -1;
+            dato = dato.Replace(".", "");
+            if (dato.Length < 1 || dato.Length > 8 || !int.TryParse(dato, out dni))
+            {
+                throw new DniInvalidoException();
+            }
+            return this.ValidarDni(nacionalidad, dni);
+        }
+
+        private string ValidarNombreApellido(string dato)
+        {
+            string aux = "";
+            if (Regex.IsMatch(dato, @"^[a-zA-Z\s]+$"))
+            {
+                aux = dato;
+            }
+            return aux;
+        }
+
+    }
+}
